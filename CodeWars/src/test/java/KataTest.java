020fa8b75cf09400a54188d0ae978160cc3a7f67
Denis Fesenko <dfesek1@gmail.com>
2021-05-31 12:54:53 +0300

    Add Code-Wars done tasks

---
+++ CodeWars/src/test/java/KataTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class KataTest {
+    private double precision = 0.0000000000001;
+
+    @Test
+    public void sampleTestCases() {
+        assertEquals(1.0, Kata.findUniq(new double[]{0, 1, 0}), precision);
+        assertEquals(2.0, Kata.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
+        assertEquals(55.0, Kata.findUniq(new double[]{55, 1, 1, 1, 1, 1}), precision);
+        assertEquals(0.0, Kata.findUniq(new double[]{0, 1, 1, 1, 1, 1}), precision);
+    }
+}
